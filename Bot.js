c67168dc540faa2ac7cdad0e4c11ea62f17ce6af
DuvansX <duvanchaparro3344@gmail.com>
2025-04-14 21:53:36 -0500

    Add files via upload

---
+++ Bot.js
@@ -0,0 +1,15 @@
+type="text/javascript">
+  (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '67e718ea35cfb3ecc9d6d682' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
+          }
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
