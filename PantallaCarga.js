039e1748b787a5f8bd5c5912ce97a2c93eecbfad
DuvansX <duvanchaparro3344@gmail.com>
2025-05-06 18:16:34 -0500

    Add files via upload

--- PantallaCarga.js
+++ PantallaCarga.js
@@ -1,12 +1,20 @@
+// Esperar a que todos los recursos se hayan cargado
 window.addEventListener("load", function() {
-
+    // Desactivar el bloqueo de interacción
     document.body.classList.remove("locked");
 
+    // Ocultar la pantalla de carga
     let loadingScreen = document.getElementById("loading-screen");
     loadingScreen.classList.add("hidden");
-    setTimeout(() => {
-        loadingScreen.style.display = "none";
-    }, 1000);
-});
 
-document.body.classList.add("locked");
+    // Desaparecer completamente después de la animación
+    setTimeout(() => {
+      loadingScreen.style.display = "none";
+    }, 1000);
+    
+    // Mostrar el contenido de la página
+    document.getElementById("contenido").classList.remove("hidden");
+  });
+
+  // Bloquear la interacción del usuario al cargar
+  document.body.classList.add("locked");
