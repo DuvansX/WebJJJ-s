e849ab5c9664e6f0611d28d59289a76057f17ea2
DuvansX <duvanchaparro3344@gmail.com>
2025-05-24 21:54:27 -0500

    Add files via upload

--- CategoriasCigarreria.js
+++ CategoriasCigarreria.js
@@ -48,3 +48,20 @@ window.onload = function() {
         });
     });
 };
+
+  // Obtener el botón
+  const btnSubir = document.getElementById("btnSubir");
+
+  // Mostrar el botón cuando se baja 100px
+  window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      btnSubir.style.display = "block";
+    } else {
+      btnSubir.style.display = "none";
+    }
+  };
+
+  // Al hacer clic, volver arriba suavemente
+  btnSubir.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
